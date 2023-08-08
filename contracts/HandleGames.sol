1a5c86c8f6ff3628e285c495dbfb689cbfa076fa
TheAsel <theasel99@gmail.com>
2023-08-08 16:57:14 +0200

    handlegames implementation

--- contracts/HandleGames.sol
+++ contracts/HandleGames.sol
@@ -1,6 +1,96 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.8.0 <0.9.0;
+
+import "./Battleship.sol";
 
 contract HandleGames {
-  
+
+  // ---- Local variables
+  // maps a game with its joinable status
+  mapping(Battleship => bool) private joinableGames;
+  // maps a game with its corresponding index
+  mapping(Battleship => uint256) private gamesIndex;
+  // array with the addresses of the joinable games
+  Battleship[] private gamesArray;
+  // nonce used in the random function
+  uint256 private nonce;
+
+  // ---- Events
+  // emitted when a game is created
+  event GameCreated(Battleship gameId, address indexed from);
+  // emitted when a player joins a game
+  event GameJoined(Battleship indexed gameId, address indexed by);
+  // emitted when a game isn't found
+  event GameNotFound(address indexed player);
+
+  // ---- Modifiers
+  // requirements for a game to be joinable
+  modifier joinable(Battleship game) {
+    require(joinableGames[game] == true, "This game isn't joinable");
+    require(msg.sender != game.playerOne(), "You created this game");
+    _;
+  }
+
+  // checks if there are available games
+  modifier gamesAvailable() {
+    require(gamesArray.lenght > 0, "No games available");
+    _;
+  }
+
+  // ---- External functions
+  // creates a new game
+  function createGame() external {
+    Battleship newGame = new Battleship(msg.sender);
+    joinableGames[newGame] = true;
+    gamesArray.push(newGame);
+    gamesIndex(newGame) = gamesArray.lenght - 1;
+    emit GameCreated(newGame, msg.sender);
+  }
+
+  // joins a game by its ID
+  function joinIdGame(Battleship game) external joinable(game) {
+    game.addPlayerTwo(msg.sender);
+    removeGame(game);
+    emit GameJoined(game, msg.sender);
+  }
+
+  // joins a random game from the list of joinable games
+  function joinRandomGame() external gamesAvailable() {
+    uint256 index = random();
+    bool found = false;
+    for(uint256 i = 0; i < gamesArray.lenght; i++) {
+      Battleship game = gamesArray[(index + i) % gamesArray.lenght];
+      if(game.playerOne() != msg.sender) {
+        found = true;
+        game.addPlayerTwo(msg.sender);
+        removeGame(game);
+        emit GameJoined(game, msg.sender);
+        break;
+      }
+    }
+    if(!found) {
+      emit GameNotFound(msg.sender);
+    }
+  }
+
+  // ---- Internal functions
+  // removes a game from the list of joinable games
+  function removeGame(Battleship game) internal {
+    delete joinableGames[game];
+    uint256 index = gamesIndex[game];
+    if(index == gamesArray.lenght - 1 || gamesArray.lenght == 1) {
+      gamesArray.pop();
+    } else {
+      gamesArray[index] = gamesArray[gamesArray.lenght - 1];
+      gamesArray.pop();
+      gamesIndex[gamesArray[index]] = index;
+    }
+    delete gamesIndex[game];
+  }
+
+  // returns a random value of gamesArray.lenght
+  function random() internal view returns (uint256) {
+    nonce++;
+    return uint256(keccak256(abi.encodePacked(block.difficulty, now, nonce))) % gamesArray.lenght;
+  }
 }
